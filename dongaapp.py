7be3ae303ba2675644c3a3f5c9f3b85e3a3fed04
Lwazi Mavumbuka <mvmlwa003@myuct.ac.za>
2024-12-21 00:58:34 +0200

    done transcribing

--- dongaapp.py
+++ dongaapp.py
@@ -1,10 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from requests import post, get
+import requests
+from moviepy import VideoFileClip
+import os
+import whisper
 
 dongaapp = Flask(__name__)
 
+UPLOAD_FOLDER = 'C:/Users/Lenovo/Documents/Uploads/'
+dongaapp.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+files = []
+
 @dongaapp.route("/")
 def home():
+    files.clear()
     return render_template('index.html')
+
+@dongaapp.route("/display-file", methods=['POST'])
+def displayfile():
+    file = request.files['file']
+
+    if 'file' not in request.files:
+        print('No file found')
+
+    file_path = f"{dongaapp.config['UPLOAD_FOLDER']}{file.filename}"
+    file.save(file_path)
+    file_name = file.filename
+
+    filesize = round(os.path.getsize(file_path) / 1024, 2)
+
+
+    if(filesize<1024):
+        file_str = str(filesize)
+        size = file_str + " KB"
+
+        files.append({
+            'name': file_name,
+            'size': size,
+            'path': file_path
+        })
+        print(size)
+    else:
+        filesize = round(filesize/(1024),2)
+        file_str = str(filesize)
+        size = file_str + " MB"
+
+        files.append({
+            'name': file_name,
+            'size': size,
+            'path': file_path
+        })
+        print(size)    
+
+    #clip = VideoFileClip('C:/Users/Lenovo/Documents/videoplayback.mp4')
+    #clip.audio.write_audiofile(f"{dongaapp.config['UPLOAD_FOLDER']}audio.mp3")
+    #print(file_path)
+    return jsonify(files)
+
+@dongaapp.route("/generate_audio", methods=['POST'])
+def generate_audio():
+    filepath = request.json
+
+    clip = VideoFileClip(filepath)
+    clip.audio.write_audiofile(f"{dongaapp.config['UPLOAD_FOLDER']}audio.mp3")
+
+    model = whisper.load_model("tiny")
+    result = model.transcribe(f"{dongaapp.config['UPLOAD_FOLDER']}audio.mp3")
+    print(result['text'])
+
+    print(filepath)
+    return ''
 
 if __name__ == '__main__':
     dongaapp.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -3,4 +3,67 @@ const fileInput = form.querySelector(".file-input");
 
 form.addEventListener("click", () =>{
     fileInput.click();
+    showfile()
 });
+
+let filepath =[]
+
+fileInput.addEventListener("change", async () => {
+    const file = fileInput.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const response = await fetch('/display-file', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            const files = await response.json();
+            const filesDiv = document.getElementById('files');
+            filesDiv.innerHTML = '';
+
+            files.forEach(file => {
+                const uploaded = document.createElement('div');
+                uploaded.classList.add('uploaded');
+                uploaded.innerHTML = `
+                    <li class="row">
+                        <div class="content">
+                            <i class='bx bxs-file'></i>
+                            <div class="details">
+                                <span class="name">${file.name} • Uploaded</span>
+                                <span class="size">${file.size}</span>
+                            </div>
+                        </div>
+                        <i class='bx bx-check'></i>
+                    </li>`;
+                    filepath.push(file.path)
+                filesDiv.append(uploaded);
+            });
+        } else {
+            console.error('Failed to upload file:', await response.json());
+        }
+    } else {
+        console.error('No file selected.');
+    }
+});
+
+async function get_notes(){
+    query = filepath[0]
+
+    const response = await fetch('/generate_audio', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(query)
+    })
+
+    const notes = await response.json();
+}
+
+
+
+
